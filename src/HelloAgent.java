16c9901fe71f8a2b86ccce57134cf2f5e809d9df
Eleazar Meza <meza.eleazar@gmail.com>
2014-05-10 00:22:50 -0430

    Agregado serialVersionUID para eliminar advertencia del compilador

--- src/HelloAgent.java
+++ src/HelloAgent.java
@@ -1,7 +1,11 @@
-
 import jade.core.*;
 
 public class HelloAgent extends Agent {
+    /**
+     * Serial para que Eclipse deje deje joder con la advertencia
+     */
+    private static final long serialVersionUID = 1520345756839421693L;
+
     protected void setup()  {
         System.out.println("Hello World!");
     }
