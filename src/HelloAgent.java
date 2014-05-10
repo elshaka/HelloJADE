862c27374c5fc7d441d8e9da9b8b333cb0509f7d
Eleazar Meza <meza.eleazar@gmail.com>
2014-05-09 21:50:34 -0430

    Commit inicial

---
+++ src/HelloAgent.java
@@ -0,0 +1,11 @@
+import jade.core.*;
+
+public class HelloAgent extends Agent {
+    protected void setup()  {
+        System.out.println("Hello World!");
+    }
+
+    protected void takeDown() {
+        System.out.println("Goodbye World!");
+    }
+}
