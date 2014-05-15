1655b63e16cbff5e83fba1264d313903a7d4f6a3
Stefan <stefan.bullones@gmail.com>
2014-05-15 13:17:51 -0430

    Ventana Planificador - Pequeña modificacion

--- src/PlanificadorGUI.java
+++ src/PlanificadorGUI.java
@@ -18,25 +18,9 @@ public class PlanificadorGUI extends JFrame {
     private JPanel contentPane;
 
     /**
-     * Launch the application.
-     */
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    PlanificadorGUI frame = new PlanificadorGUI();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    /**
      * Create the frame.
      */
-    public PlanificadorGUI() {
+    public PlanificadorGUI(Planificador planificador) {
         setTitle("Planificador");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 250, 142);
