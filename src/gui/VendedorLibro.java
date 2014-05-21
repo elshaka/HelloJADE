70a54403bd00111fbb00a3e0e839395ece2a0bcb
Eleazar Meza <meza.eleazar@gmail.com>
2014-05-21 13:45:13 -0430

    Eliminados imports sin utilizar

--- src/gui/VendedorLibro.java
+++ src/gui/VendedorLibro.java
@@ -19,8 +19,6 @@ import javax.swing.BoxLayout;
 import java.awt.Rectangle;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 @SuppressWarnings("serial")
 public class VendedorLibro extends JDialog {
